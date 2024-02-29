53c20db4eb38e5699a05c0dffd6d2a74e1734f69
Xyfalix <129175727+Xyfalix@users.noreply.github.com>
2024-02-29 16:26:45 +0800

    chore: refactor database.ts code to typescript

--- config/database.ts
+++ config/database.ts
@@ -1,10 +1,10 @@
-const mongoose = require("mongoose");
-const debug = require("debug")("mern:config:database");
+import mongoose, { Connection } from "mongoose";
+import debug from "debug";
 
 mongoose.set("debug", true);
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.DATABASE_URL as string)
 
-const db = mongoose.connection;
+const db: Connection = mongoose.connection;
 
 db.on("connected", function () {
   debug(`Connected to ${db.name} at ${db.host}:${db.port}`);
